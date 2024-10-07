5c0fc5d9157a1bab0d17a6226d6af813add6b431
Nanu25 <alexandrugrancea25@gmail.com>
2024-10-07 15:15:13 +0300

    working on design

--- project4/settings.py
+++ project4/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    # 'default': dj_database_url.config(
+    #     default=os.environ.get('DATABASE_URL')
+    # )
 }
 
 AUTH_USER_MODEL = "network.User"
